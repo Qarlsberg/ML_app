8304fcdf7ada611ca1b927a54e6f8d0c01138481
Anton Åkerling <121064657+Qarlsberg@users.noreply.github.com>
2024-02-27 20:46:16 +0100

    Final before presentation

--- app.py
+++ app.py
@@ -12,38 +12,33 @@ import logging
 from typing import Optional
 from requests.packages.urllib3.util.retry import Retry
 
+from sentence_transformers import SentenceTransformer
+import torch
+
+headers = {'bypass-tunnel-reminder': 'any_value'}
+response = requests.get('http://akabi.loca.lt', headers=headers)
+
 ## page settings
 st.set_page_config(page_title="AmpleCart Feedback App", layout="wide")
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-     
-    """,
-    unsafe_allow_html=True,
-)
-# ChromaDB client
-chroma_client = chromadb.HttpClient(host='localhost', port=8000)
-heartbeat = chroma_client.heartbeat()
-if not heartbeat:
-    st.error("Failed to connect to ChromaDB. Please check the logs for more details.")
-    st.stop()
-collection = chroma_client.get_collection(name=
-        "SentimentAnalysis_3")
-collection_count = collection.count()
-if not collection:
-    st.error("Failed to get the collection. Please check the logs for more details.")
-    st.stop()
 
-st.sidebar.write("Collection count:", collection_count)
+# ChromaDB client
+@st.cache_resource
+def get_database_client():
+    chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+    heartbeat = chroma_client.heartbeat()
+    if not heartbeat:
+        st.error("Failed to connect to ChromaDB. Please check the logs for more details.")
+        st.stop()
+    collection = chroma_client.get_collection(name="SentimentAnalysis_3")
+
+    if not collection:
+        st.error("Failed to get the collection. Please check the logs for more details.")
+        st.stop()
+
+    return collection
+
 # Set the title of the page
-st.sidebar.title('AmpleCart feedback analysis app')
+st.title('AmpleCart feedback analysis app')
 
 # Add some text to describe your app
 st.sidebar.write('This app queries a subset of reviews using similarity search.')
@@ -52,25 +47,23 @@ st.sidebar.write('This app queries a subset of reviews using similarity search.'
 #st.write('Database query results and visualizations will appear here.')
 
 #api_token = st.text_input('Enter your API token here)', 'API token')
-api_token = 'api_token'
-
-# Example query
-
-# Create a text input widget for entering the query
-query_text = st.sidebar.text_input('Enter your query here:', 'The shipping was') 
-
-# Create a slider for selecting the number of query results to return
-knum = st.sidebar.slider("Select amount of query results", 1, 2000, value=10, step=50)
-
 # Load the Excel file with the topic mappings
 topic_mapping_path = 'Topic Categories Mapping.xlsx'
 topic_mappings = pd.read_excel(topic_mapping_path)
 
+
 # Get the categorys from the topic mappings
 categories = topic_mappings['Category'].unique()
 
-# Create a multiselect widget for selecting categories
+# Example query
+
+
+# Create a text input widget for entering the query
+query_text = st.sidebar.text_input('Enter your query search here:', 'The shipping was') 
+
+knum = st.sidebar.slider("Select amount of query results", 100, 4000, value=500, step=100)
 selected_categories =st.sidebar.multiselect("Select one, many or no Categories",categories)
+
 
 # Create a 'where' clause for the query based on the selected categories
 if len(selected_categories) == 1:
@@ -81,23 +74,37 @@ else:
     # If no categories are selected, you can choose to omit the 'where' clause or tailor it as needed
     where_clause = {}
 
+
+@st.cache_resource
+def model_load():
+    return SentenceTransformer("all-MiniLM-L6-v2", device=device)
+# set device to GPU if available
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# use a sentence transformer model to encode the query text
+retriver = model_load()
+
+progress_text = "Operation in progress. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+
+query_embedding = retriver.encode(query_text).tolist()
+
 # API token
-api_token = "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"
+#api_token = 
 
-API_TOKEN = api_token
+#API_TOKEN = api_token
 
-embedding_cache = {}
-API_URL = "https://api-inference.huggingface.co/models/BAAI/bge-small-en-v1.5"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-query_embedding = query(query_text)
+#embedding_cache = {}
+#API_URL = "https://api-inference.huggingface.co/models/BAAI/bge-small-en-v1.5"
+#headers = {"Authorization": f"Bearer {API_TOKEN}"}
+#def query(payload):
+#    response = requests.post(API_URL, headers=headers, json=payload)
+#    return response.json()
+#query_embedding = query(query_text)
 
 
-#st.write("Checkpoint: After embedding")
-#st.write(embedding)
-
+# Show and update progress bar
+collection = get_database_client()
 # query embedding function
 text_query = collection.query(
     query_embeddings=query_embedding,
@@ -106,10 +113,13 @@ text_query = collection.query(
     where=where_clause
 )
 
-# Show and update progress bar
-bar = st.sidebar.progress(50)
-time.sleep(2)
-bar.progress(100)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+
 
 rows = []
 # Assuming each key's first list item contains lists of equal length
@@ -129,7 +139,11 @@ for i in range(len(text_query['ids'][0])):
     rows.append(row)
 
 # Create a DataFrame from the list of dictionaries
-df = pd.DataFrame(rows)
+@st.cache_data
+def get_dataframe():
+    return pd.DataFrame(rows)
+df = get_dataframe()
+
 
 # Sidebar for page selection using radio buttons
 #page = st.radio('Choose a page:', ['Dataframe', 'Visuals'])
@@ -137,6 +151,7 @@ df = pd.DataFrame(rows)
 tab1, tab2 = st.tabs(["Dataframe", "Visuals"])
 
 topic_mappings['Category'].unique()
+
 
 ## visualizations code
 
@@ -163,35 +178,103 @@ if len(selected_categories) > 5:
 
 # Aggregate data for overall sentiment counts (ignoring categories)
 overall_sentiment_counts = df.groupby('sentiment').size().reset_index(name='count')
+overall_sentiment_counts['sentiment'] = overall_sentiment_counts['sentiment'].apply(lambda x: f"{x} ★")
+
+# mean sentiment score for each category and overall
+mean_sentiment = df.groupby('category')['sentiment'].mean().reset_index(name='mean_sentiment')
+overall_mean_sentiment = df['sentiment'].mean()
+# filtered mean sentiment score for each category
+filtered_data = df[df['category'].isin(selected_categories)]
+filtered_mean_sentiment = filtered_data.groupby('category')['sentiment'].mean().reset_index(name='mean_sentiment')
+#filtered_mean_sentiment['mean_sentiment'] = filtered_mean_sentiment['mean_sentiment'].apply(lambda x: f"{x:.1f} ★")
+
+# Average sentiment score set by the whole dataset
+avg_sentiment_score_wholeset = 3.20
+
+# Function to get delta score 
+def get_delta_score_overall(avg_sentiment_score_wholeset, overall_mean_sentiment):
+    delta_score = overall_mean_sentiment - avg_sentiment_score_wholeset
+    return delta_score
+
+def get_delta_score_category(overall_mean_sentiment, category_mean_sentiment):
+    delta_score = category_mean_sentiment - overall_mean_sentiment
+    if delta_score < 0:
+        # Negative change, should be red and downward arrow
+        return f"{delta_score:.2f}", "normal"  # Assuming 'negative' turns text red
+    else:
+        # Positive change, should be green and upward arrow
+        return f"{delta_score:.2f}", "normal"  # Assuming 'positive' turns text green
+
+
+
+# Create a dataframe for the ag grid
+df_dataframe = df[['document', 'category', 'sentiment']]
+
+# add star to the sentiment
+df_dataframe['sentiment'] = df_dataframe['sentiment'].apply(lambda x: f"{x} ★")
 
 # Display the selected page
 
 with tab1:
-    st.spinner('Loading query results...')
-    st.title('A DataFrame of the query results')
-    st.dataframe(df, use_container_width=True)
+    col1, col2 = st.columns([2,1],gap="small")
+    col1.title('A DataFrame of the query results')
+    col1.dataframe(df_dataframe,use_container_width=True,height=500, hide_index=True, 
+                   column_config={'document': {'width': 500}})
+    # Title for the plot
+    # Title for the plot
+    col2.title('Top 5 Categories by Count')
+
+    # Calculate the top 5 categories
+    top_topics = df['category'].value_counts().head(5).reset_index()
+    top_topics.columns = ['Category', 'Count']
+    top_topics = top_topics.sort_values('Count', ascending=True)
+
+    # Create a horizontal bar chart with Plotly Express
+    
+    fig = px.bar(top_topics, y='Category', x='Count', orientation='h')
+
+    # Place the category labels after the bars
+    fig.update_traces(textposition='outside')
+
+    # Display the plot in the Streamlit app
+    col2.plotly_chart(fig)
 
 with tab2:
-    st.spinner('Loading visualizations...')
     st.title('Visuals Page')
+    
+    cols = st.columns(len(selected_categories)+1)
+    with cols[0]:  # This will display the overall sentiment score in the first column
+        st.metric(label="Sentiment Score", 
+                  value=f"{overall_mean_sentiment:.2f} ★", 
+                  delta=f"{get_delta_score_overall(avg_sentiment_score_wholeset, overall_mean_sentiment):.2f} ★",
+                  help="The average sentiment score for the whole dataset is 3.20 ★")
+
+    if not selected_categories:  # This should be fine if selected_categories is a list
+        # if no category is selected, display nothing
+        st.empty()
+    else:
+        for i, category in enumerate(selected_categories, start=1):  # Start from 1 to leave the first column for the overall score
+            with cols[i]:  # This will place each category score in its own column
+                category_mean_sentiment = filtered_mean_sentiment[filtered_mean_sentiment['category'] == category]['mean_sentiment'].iloc[0]
+                delta, delta_color = get_delta_score_category(overall_mean_sentiment, category_mean_sentiment)
+                st.metric(
+                    label=f"{category} Sentiment Score", 
+                    value=f"{category_mean_sentiment:.2f} ★", 
+                    delta=delta,
+                    delta_color=delta_color)
     col1, col2 = st.columns(2)
-        
     # Sentiment counts plot (overall, not filtered by category)
-    col1.write('Overall Sentiment Counts')
+
     sentiment_fig = px.bar(overall_sentiment_counts, x='sentiment', y='count',
                                title='Overall Sentiment Counts',
                                labels={'sentiment': 'Sentiment', 'count': 'Count'})
     col1.plotly_chart(sentiment_fig, use_container_width=True)
         
     # Distribution plots for each selected category
-    col2.write('Sentiment Distribution by Category (Top 5)')
     if not selected_categories:  # This should be fine if selected_categories is a list
         col2.write('No category selected. Please select a category to view this chart.')
     else:
-        filtered_data = df[df['category'].isin(selected_categories)]
-
         category_sentiment_counts = filtered_data.groupby(['category', 'sentiment']).size().reset_index(name='count')
-         # Ensure sentiments are labeled with stars in the plot
         category_sentiment_counts['sentiment'] = category_sentiment_counts['sentiment'].apply(lambda x: f"{x} ★")
     
         sentiment_distribution_fig = px.bar(category_sentiment_counts, x='sentiment', y='count',
@@ -199,3 +282,4 @@ with tab2:
                                     title='Sentiment Distribution by Category (Top 5)',
                                     labels={'count': 'Count', 'sentiment': 'Sentiment', 'category': 'Category'})
         col2.plotly_chart(sentiment_distribution_fig, use_container_width=True)
+        
